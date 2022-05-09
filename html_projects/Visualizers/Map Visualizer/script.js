2d640ca26a376fd5e0cc4cc94b3cab06c3150e90
Nav <navaneeth.mailid@gmail.com>
2022-05-08 19:27:54 -0700

    html projects

---
+++ html_projects/Visualizers/Map Visualizer/script.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmF2ZCIsImEiOiJja3FhNTA0MnAwMjZ6Mm9vaGkxMDd4NDBqIn0.6GxwkZc_w7r-phacF_zCsw';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+  })
+  
+  function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+  }
+  
+  function errorLocation() {
+    setupMap([-2.24, 53.48])
+  }
+  
+  function setupMap(center) {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: center,
+      zoom: 15
+    })
+  
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+  
+    var directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+    })
+  
+    map.addControl(directions, "top-left")
+  }
